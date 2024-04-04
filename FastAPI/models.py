27fda3ea6383b730cedc92e37fa01aaa01c0b66e
m1siraj <50576588+m1siraj@users.noreply.github.com>
2024-04-03 20:36:50 -0400

    Update models.py

--- FastAPI/models.py
+++ FastAPI/models.py
@@ -1,13 +1,30 @@
-from database import Base
-from sqlalchemy import Column, Integer, String, Boolean, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
-class Transaction(Base):
-    __tablename__ = 'transactions'
+class Product(Base):
+    __tablename__ = 'products'
 
     id = Column(Integer, primary_key=True, index=True)
-    amount = Column(Float)
-    category = Column(String)
-    description = Column(String)
-    is_income = Column(Boolean)
-    date = Column(String)
+    name = Column(String, index=True)
+
+class Warehouse(Base):
+    __tablename__ = 'warehouses'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    max_capacity = Column(Integer)
+    current_capacity = Column(Integer)
+
+class DeliveryVehicle(Base):
+    __tablename__ = 'delivery_vehicles'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    range = tuple(Integer, Integer)
+    capacity = Column(Integer)
+    
